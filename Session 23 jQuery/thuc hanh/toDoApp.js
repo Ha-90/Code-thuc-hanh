19a990edd5ba8c084bc6f55b6e5219257353b61e
Ha 90 <hoangha2h@gmail.com>
2022-12-14 10:56:32 +0700

    update

--- Session 23 jQuery/thuc hanh/toDoApp.js
+++ Session 23 jQuery/thuc hanh/toDoApp.js
@@ -2,11 +2,12 @@
     function drawTable(){
         let todoItem = JSON.parse(localStorage.getItem('todoItem'));
         let data = "";
+        console.log("111",todoItem);
         for(let i = 0; i < todoItem.length; i++){
             data += `
             <tbody id="listTodo">
             <td>${i+1}</td>
-            <td>${todoItem[i].item}</td>
+            <td class="todo-item">${todoItem[i].item}</td>
             <td>${todoItem[i].status}</td>
             <td col-span="3"><button id="edit" onclick="edit(${i})">EDIT</button>
             <button id="delete" onclick="del(${i})">DELETE</button>
@@ -26,22 +27,35 @@
             saveList();
         }
     });
-
+    
     function saveList(){
         let input = document.getElementById("input");
+        let keyId = JSON.parse(localStorage.getItem("keyId"));
+        if (keyId != null){
+        let todoItem = JSON.parse(localStorage.getItem('todoItem'));   
+            todoItem[keyId].item = input.value;
+            todoItem[keyId].status = "In Process";
+            todoItem.splice(keyId, 1, todoItem[keyId]);
+            localStorage.setItem('todoItem', JSON.stringify(todoItem));
+            document.getElementById("btnSave").innerHTML = "SAVE";
+            input.value = "";
+            drawTable();
+            localStorage.removeItem("keyId");
+            return;
+        } 
         if (input.value == ""){
+            alert("Vui lòng nhập dữ liệu");
             return false;
         }
-        let todoItem = JSON.parse(localStorage.getItem('todoItem'));
-        
+        let todoItem = JSON.parse(localStorage.getItem('todoItem'));      
         if(todoItem == null) {
             let todoArr = [
                 {
                     item: input.value,
                     status: "In Process",
+                    
                 }
             ];
-            // todoArr.push(input.value);
             localStorage.setItem('todoItem', JSON.stringify(todoArr));
         }
         else {
@@ -49,8 +63,7 @@
                 {
                    item: input.value,
                    status: "In Process", 
-                }
-            
+                }          
             todoItem.push(item);
             localStorage.setItem('todoItem', JSON.stringify(todoItem));
         }
@@ -58,6 +71,7 @@
     drawTable();
     }
 
+    
 // Tính năng xóa to do
     function del(i){
         let todoItem = JSON.parse(localStorage.getItem('todoItem'));     
@@ -77,9 +91,11 @@
 // Tính năng edit 
     function edit(i){
         let todoItem = JSON.parse(localStorage.getItem('todoItem')); 
-        todoItem[i].status = "In Process";
-        localStorage.setItem('todoItem', JSON.stringify(todoItem));
-        drawTable();
+        let input = document.getElementById("input");
+        
+        input.value = todoItem[i].item;
+        document.getElementById("btnSave").innerHTML = "EDIT";      
+        localStorage.setItem('keyId', JSON.stringify(i));       
     }
 
 
